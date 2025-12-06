b174ff2125937c44a37a372cc80207a258e167ac
HimanM <67066047+HimanM@users.noreply.github.com>
2025-12-06 13:03:15 +0530

    feat: add Terraform outputs for EKS cluster details and Istio Ingress Gateway hostname to cluster-a and cluster-b.

--- infra/cluster-b/outputs.tf
+++ infra/cluster-b/outputs.tf
@@ -13,12 +13,27 @@ output "region" {
   value       = var.region
 }
 
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
 
 output "load_balancer_hostname" {
-  value       = one(data.kubernetes_service.ingress_gateway.status.0.load_balancer.0.ingress).hostname
+  value       = try(data.kubernetes_service.ingress_gateway.status.0.load_balancer.0.ingress.0.hostname, "pending... wait a few mins and run 'kubectl get svc -n istio-system istio-ingressgateway'")
   description = "The external hostname of the Istio Ingress Gateway LoadBalancer"
 }
